3d2e60232dbadf323ed9525c1923ceb9aba87d95
Will Sabi Jr. <geekyadventurerr@gmail.com>
2026-05-11 16:49:41 +0000

    debut enquiry modal

--- layout.js
+++ layout.js
@@ -1,28 +1,80 @@
 function getBrand() {
-  return "byd"; // Single-brand site
+
+return "byd";
 }
 
 const brand = getBrand();
 
-// ===== HEADER =====
 const headerHTML = `
 <header>
-  <img src="logo.jpg" alt="Zerosol Logo" class="logo">
+
+  <img src="logo.jpg"
+       alt="Zerosol Logo"
+       class="logo">
 
   <h1 class="showroom-title">
-    <img src="images/logos/${brand}.png" class="title-logo">
-    BYD Showroom
+
+    ${
+      brand
+        ? `<img src="images/logos/byd.png"
+                 class="title-logo">`
+        : ""
+    }
+
+    ${
+      brand
+        ? brand === "byd"
+          ? "BYD"
+          : brand.charAt(0).toUpperCase() + brand.slice(1)
+        : ""
+    }
+
+    Showroom
+
   </h1>
 </header>
 `;
 
+const footerHTML = `
+<footer>
+
+  <p>
+    Contact us:
+    <a href="mailto:sales@zerosolafrica.co">
+      sales@zerosolafrica.co
+    </a>
+  </p>
+
+  <p>
+    ☎
+    <a href="tel:+233595445544">
+      +233 59 544 5544
+    </a>
+    |
+
+    <a href="tel:+233595444454">
+      +233 59 544 4454
+    </a>
+    |
+
+    <a href="tel:+17348833934">
+      +1 (734) 883-3934
+    </a>
+  </p>
+
+  <p>
+    &copy;
+    <span id="year"></span>
+    Zerosol Automotive.
+    All rights reserved.
+  </p>
+
+</footer>
+`;
+
 document.getElementById("header-placeholder").innerHTML = headerHTML;
 
+document.getElementById("footer-placeholder").innerHTML = footerHTML;
 
-// ===== FOOTER =====
-fetch("footer.html")
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById("footer-placeholder").innerHTML = html;
-    document.getElementById("year").textContent = new Date().getFullYear();
-  });
+document.getElementById("year").textContent =
+  new Date().getFullYear();
